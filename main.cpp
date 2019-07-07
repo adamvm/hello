3154d49b30cd2ef0d3f8e5d0c5684cfe3070f27e
Adam Mierzwiak <adam.mierzwiak@gmail.com>
2019-07-07 13:37:41 +0200

    Injected error for CI verification

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main() {
 	cout << HELLO << endl;
 	cout << "Hello Wrocław" << endl;
 	cout << "Hello Coders Shool" << endl;
+	ccout << "Good bye" << endl; # Injected error to verify CI
 
 	return 0;
 }
