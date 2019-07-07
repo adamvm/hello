358a9be5adf7f53296f6eb34a8adb0325f21482c
Adam Mierzwiak <adam.mierzwiak@gmail.com>
2019-07-07 13:44:25 +0200

    Revert "Injected error for CI verification"
    This reverts commit 3154d49b30cd2ef0d3f8e5d0c5684cfe3070f27e.

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@ int main() {
 	cout << HELLO << endl;
 	cout << "Hello Wrocław" << endl;
 	cout << "Hello Coders Shool" << endl;
-	ccout << "Good bye" << endl; # Injected error to verify CI
 
 	return 0;
 }
