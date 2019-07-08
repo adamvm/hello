70391c8fcec34140d7d744df3b8805bfadb87708
Adam <adam.mierzwiak@gmail.com>
2019-07-08 19:10:31 +0200

    Update and rename objectContainer.cpp to boostArray.cpp

---
+++ boostArray.cpp
@@ -0,0 +1,23 @@
+#include <boost/array.hpp>
+#include <algorithm>
+
+typedef boost::array<char, 4> array4_t;
+
+array4_t &vector_advance(array4_t &val) {
+    const auto inc = [](char& c) {c++;};
+
+    std::for_each(val.begin(), val.end(), inc);
+    return val;
+}
+
+int main() {
+    array4_t val = {0, 1, 2, 3};
+
+    array4_t val_res;
+    val_res = vector_advance(val);
+
+    assert(val.size() == 4);
+    assert(val[0] == 1);
+
+    assert(sizeof(val) == sizeof(char) * array4_t::static_size);
+}
